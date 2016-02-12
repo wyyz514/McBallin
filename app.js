510d2e48e5917208c72ccefda534883fa126ac08
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-02-11 19:09:52 -0500

    Added block to check for existing tokens

--- app.js
+++ app.js
@@ -6,11 +6,39 @@ var express = require("express"),
 var app = express(),
   db = require("./app/db.js"),
   calendar = google.calendar('v3'),
+  date = require('datejs'),
   auth = new googleAuth(),
   oAuthClient = new auth.OAuth2(credentials.client_id, credentials.client_secret, credentials.redirect_url),
   tokenUtils = require('./app/auth.js')(oAuthClient),
   Token = require("./app/token.js"),
   port = 3000 | process.env.PORT;
+
+function list_events(auth) {
+  calendar.events.list({
+  auth: auth,
+  calendarId: 'athleticsmcgill@gmail.com',
+  maxResults: 10,
+  singleEvents: true,
+  orderBy: 'startTime',
+  timeMin:new Date().toISOString()
+}, function(err, response) {
+  if (err) {
+    console.log('The API returned an error: ' + err);
+    return;
+  }
+  var events = response.items;
+  if (events.length == 0) {
+    console.log('No upcoming events found.');
+  } else {
+    console.log('Upcoming 10 events:');
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      var start = event.start.dateTime || event.start.date;
+      console.log('%s - %s', Date.parse(start).toDateString(), event.summary);
+    }
+  }
+});
+}
 
 app.get('/', function(req, res) {
     Token.findOne({},function(err,tokens){
@@ -18,6 +46,7 @@ app.get('/', function(req, res) {
         // If going through here always refresh
         tokenUtils.refreshToken(tokens.refresh_token);
         res.send('authenticated');
+        list_events(oAuthClient);
       } else {
         tokenUtils.requestToken(res);
       }
@@ -39,6 +68,12 @@ app.get('/auth', function(req, res) {
     });
   }
 });
+
+// make sure user is authenticated but check for existing tokens first
+ Token.findOne({},function(err,token) {
+   if(token)
+    tokenUtils.authenticateWithDB();
+ });
 
 app.listen(port,function(){
   console.log("Listening on port "+port);
